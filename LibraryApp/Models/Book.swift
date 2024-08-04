6e7348f11c3125376144bbd314e5917b440815cb
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-04 13:17:52 +0330

    feat: Add `Book` file to the new `Models` directory
        - Defined Category enum with various book categories sorted alphabetically
        - Defined Book struct with title, author, and category properties
        - Defined BookProtocol to specify the properties for a book

---
+++ LibraryApp/Models/Book.swift
@@ -0,0 +1,45 @@
+//
+//  Book.swift
+//  LibraryApp
+//
+//  Created by Mahdi on 5/14/1403 AP.
+//
+
+import Foundation
+
+enum Category: String {
+    
+    case art                = "Art"
+    case biography          = "Biography"
+    case children           = "Children"
+    case cooking            = "Cooking"
+    case drama              = "Drama"
+    case fantasy            = "Fantasy"
+    case fiction            = "Fiction"
+    case health             = "Health"
+    case history            = "History"
+    case horror             = "Horror"
+    case mystery            = "Mystery"
+    case nonFiction         = "Non-Fiction"
+    case poetry             = "Poetry"
+    case religion           = "Religion"
+    case romance            = "Romance"
+    case science            = "Science"
+    case scienceFiction     = "Science Fiction"
+    case selfHelp           = "Self-Help"
+    case thriller           = "Thriller"
+    case travel             = "Travel"
+}
+
+protocol BookProtocol {
+    
+    var title: String { get }
+    var author: String { get }
+    var category: Category { get }
+}
+
+struct Book: BookProtocol {
+    
+    let title, author: String
+    let category: Category
+}
