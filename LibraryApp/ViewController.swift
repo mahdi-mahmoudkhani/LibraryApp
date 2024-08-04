c6f1d116c47d3e6a7a0aa7b1473ef92be6216e6f
Mahdi <125703793+mahdi-mahmoudkhani@users.noreply.github.com>
2024-08-04 14:02:21 +0330

    feat(ViewController): Add books management array and insert function
        - Added `bookList` array to store books.
        - Added `addBook()` method to add books only if they are not already present in the list.
        - Implemented a check to prevent adding duplicate books based on title, author, and category.

--- LibraryApp/ViewController.swift
+++ LibraryApp/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private var bookList: [Book] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    private func addBook(title: String!, author: String!, category: Category!) {
+        
+        guard !bookList.contains(where: { $0.title == title &&
+                                          $0.author == author &&
+                                          $0.category == category } )
+        else {
+            return
+        }
+        bookList.append( Book(title: title, author: author, category: category) )
+    }
 
 }
 
